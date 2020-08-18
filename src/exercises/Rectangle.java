cff65b27d4f34b827e4365e2621c29a32fd09c10
Corinne Seckinger <cseckinger0525@gmail.com>
2020-08-17 20:16:41 -0500

    exercises

---
+++ src/exercises/Rectangle.java
@@ -0,0 +1,12 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the length of the rectangle:");
+        String length = input.nextLine();
+
+    }
+}
