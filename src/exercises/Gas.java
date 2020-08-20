09fb566ab2a8fe65a99523f19be76d4b8a558c58
Corinne Seckinger <cseckinger0525@gmail.com>
2020-08-19 20:07:08 -0500

    finish exercises

--- src/exercises/Gas.java
+++ src/exercises/Gas.java
@@ -3,4 +3,15 @@ package exercises;
 import java.util.Scanner;
 
 public class Gas {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many miles have you driven:");
+        double miles = input.nextDouble();
+
+        System.out.println("How many gallons of gas have you used:");
+        double gas = input.nextDouble();
+
+        System.out.println("Your miles per gallon is " + miles / gas);
+   input.close();
+    }
 }
--- src/exercises/Rectangle.java
+++ src/exercises/Rectangle.java
@@ -6,7 +6,13 @@ public class Rectangle {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("What is the length of the rectangle:");
-        String length = input.nextLine();
+        int length = input.nextInt();
+
+        System.out.println("What is the width of the rectangle:");
+        int width = input.nextInt();
+
+        System.out.println("The area of the rectangle is "+width*length);
+        input.close();
 
     }
 }
